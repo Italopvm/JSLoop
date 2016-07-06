8ede53ea6515b6a00c05d0af326c450fce4ecce5
italopvm <italopvm@gmail.com>
2016-07-06 12:22:04 -0600

    fisrt commit

---
+++ main.js
@@ -0,0 +1,84 @@
+/*0
+1
+2
+3
+4
+Solution:
+
+for(var i = 0; i < 5; i++){
+    console.log(i);
+}*/
+
+
+
+ /*   0 100 200 300 400 500
+1 2 4 8 16 32 64
+1 1 1 2 2 2 3 3 3
+0 2 4 6 8 10
+3 6 9 12 15
+9 8 7 6 5 4 3 2 1 0
+0 1 2 3 0 1 2 3 0 1 2 3 */
+
+
+//ex 1
+
+/*for(var i = 0; i < 6; i++) {
+	console.log(i*100);
+}*/
+
+//ex 2
+
+/*for(var i = 0; i < 7; i++){
+	console.log(Math.pow(2,i));
+
+}*/
+
+// ex 3
+
+ /*for (var  i = 1; i < 10; i++) {
+ 	for(var a = 0; a < 3; a++)
+ 		console.log(i)
+ 		}*/
+
+ //ex 4
+
+   /*for( var i = 0; i < 11; i+=2){
+ 	   console.log(i)
+       }*/
+
+ // ex 5
+
+ /*for ( var i = 3; i < 16; i+=3){
+ 	console.log(i)
+
+ }*/
+
+ //ex 6
+
+ //9 8 7 6 5 4 3 2 1 0
+
+ /*for(var i = 9; i > 0; i--){
+ 	console.log(i)
+ }*/
+//0 1 2 3 0 1 2 3 0 1 2 3 
+
+for (var i = 0; i < 3; i++){
+	for( var a = 0; a < 4; a++)
+		console.log(a)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
